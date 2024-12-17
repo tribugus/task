241be51122918aee2b181be7f89606885307f7bb
gus <gustribuna@gmail.com>
2024-12-17 13:22:38 -0600

    S

--- angular/src/app/task/components/addtarea/addtarea.component.ts
+++ angular/src/app/task/components/addtarea/addtarea.component.ts
@@ -11,31 +11,6 @@ export class AddtareaComponent implements OnInit {
 
 
 
-  tareaForm: FormGroup;
-
-  constructor(private fb: FormBuilder) {
-    // Creamos el formulario con sus validaciones
-    this.tareaForm = this.fb.group({
-      usuario_id: ['', Validators.required],
-      titulo: ['', [Validators.required, Validators.maxLength(100)]],
-      descripcion: ['', [Validators.required, Validators.maxLength(500)]],
-      fecha_inicio: ['', Validators.required],
-      fecha_vencimiento: ['', Validators.required],
-      prioridad: ['', Validators.required],
-      estado: ['', Validators.required],
-    });
-  }
-
-  // Método para manejar el envío del formulario
-  onSubmit() {
-    if (this.tareaForm.valid) {
-      console.log('Formulario Enviado:', this.tareaForm.value);
-      // Aquí puedes hacer una llamada HTTP para registrar la tarea
-    } else {
-      console.log('Formulario no válido');
-    }
-  }
-
   
   ngOnInit(): void {
   }
